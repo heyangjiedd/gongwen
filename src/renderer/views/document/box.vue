a619b74544d5766dec58870c002314bb73acc230
heyangjiedd <1059320200@qq.com>
2019-10-17 20:50:37 +0800

    同步

--- src/renderer/views/document/box.vue
+++ src/renderer/views/document/box.vue
@@ -77,10 +77,10 @@
 <!--          信函格式-->
           <div v-if="output==1" style="text-align: right;">
             <div v-if="item.items.length > 1" :style="{position: 'relative'}">
-              <span v-if="index===0" v-html="r.content2" :style="{marginRight: item.items[2].content2.split('###').length> 1?'192pt':'128pt'}"></span>
-              <span v-else-if="index===1" v-html="r.content2" :style="{float: 'right',position:'relative',marginRight:item.items[2].content2.split('###').length> 1?'112pt':'48pt',top:'-'+r.wordStyle.lineHeight}"></span>
-              <div v-else :style="{position: 'absolute',right: '0',top:'-'+r.wordStyle.lineHeight,width: r.content2.split('###').length> 1?'112pt':'42pt',textAlign: 'left'}">
-                    <span v-for="(i,j) in r.content2.split('###')" :key="i+j"
+              <span v-if="index===0" v-html="r.content2" :style="{marginRight: item.items[2].content2.split(' ').length> 1?'192pt':'128pt'}"></span>
+              <span v-else-if="index===1" v-html="r.content2" :style="{float: 'right',position:'relative',marginRight:item.items[2].content2.split(' ').length> 1?'112pt':'48pt',top:'-'+r.wordStyle.lineHeight}"></span>
+              <div v-else :style="{position: 'absolute',right: '0',top:'-'+r.wordStyle.lineHeight,width: r.content2.split(' ').length> 1?'112pt':'42pt',textAlign: 'left'}">
+                    <span v-for="(i,j) in r.content2.split(' ')" :key="i+j"
                           :style="{display: 'inline-block',width: '48pt',marginRight: j%2==0?'16pt':'0',textAlign:'justify',textAlignLast:'justify'}">{{i}}</span>
               </div>
             </div>
@@ -90,12 +90,12 @@
           <div v-else>
             <div v-if="item.items.length > 1">
               <div :style="{padding:'0 16pt',position: 'relative',
-              top:(index===0)&&item.items[2].content2.split('###').length> 2?Math.ceil(item.items[2].content2.split('###').length/2-1)*32+'pt':'',
-              paddingBottom:(index>1)&&item.items[2].content2.split('###').length> 2?Math.ceil(item.items[2].content2.split('###').length/2)*32+'pt':''}">
+              top:(index===0)&&item.items[2].content2.split(' ').length> 2?Math.ceil(item.items[2].content2.split(' ').length/2-1)*32+'pt':'',
+              paddingBottom:(index>1)&&item.items[2].content2.split(' ').length> 2?Math.ceil(item.items[2].content2.split(' ').length/2)*32+'pt':''}">
                 <span v-if="index===0" v-html="r.content2" style="float: left"></span>
-                <span v-else-if="index===1" v-html="r.content2" :style="{float: 'right',marginRight:item.items[2].content2.split('###').length> 1?'112pt':'48pt'}"></span>
-                <div v-else :style="{position: 'absolute',right: '16pt',width: r.content2.split('###').length> 1?'112pt':'48pt',textAlign: 'left'}">
-                    <span v-for="(i,j) in r.content2.split('###')" :key="i+j"
+                <span v-else-if="index===1" v-html="r.content2" :style="{float: 'right',marginRight:item.items[2].content2.split(' ').length> 1?'112pt':'48pt'}"></span>
+                <div v-else :style="{position: 'absolute',right: '16pt',width: r.content2.split(' ').length> 1?'112pt':'48pt',textAlign: 'left'}">
+                    <span v-for="(i,j) in r.content2.split(' ')" :key="i+j"
                           :style="{display: 'inline-block',width: '48pt',marginRight: j%2==0?'16pt':'0',textAlign:'justify',textAlignLast:'justify'}">{{i}}</span>
                 </div>
               </div>
